d584baab0c8ce69b9697de2a542e8166e8aa81d7
Kblv37 <kabilov.johongir@gmail.com>
2026-05-23 09:42:16 +0500

    Initial split architecture

---
+++ frontend/public/app-config.js
@@ -0,0 +1,4 @@
+window.__TWINDROP_CONFIG__ = {
+  signalingUrl: window.location.origin,
+  frontendUrl: window.location.origin
+};
--- public/app-config.js
+++
@@ -1,4 +0,0 @@
-window.__TWINDROP_CONFIG__ = {
-  signalingUrl: "http://localhost:3000",
-  frontendUrl: "http://localhost:3000"
-};
